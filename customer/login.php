569c67c6aa928a2c2387313e497c4fa8c2f61495
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-11 17:24:47 +0900

    バリデーションの追加

--- customer/login.php
+++ customer/login.php
@@ -5,7 +5,11 @@ require_once '../func/func.php';
 require_once '../func/func_db.php';
 
 $class = 'none';
-
+// バリデーションチェックを行うリスト
+$validat = [
+  'email'    => ['blank' => 1, 'email' => 1, 'unique' => 'email'],
+  'password' => ['blank' => 1, 'max_val' => 20, 'min_val' => 8]
+];
 
 // サインアップボタンを押下したときの処理
 if (!empty($_POST) && $_POST['login_btn'] === 'login') {
@@ -13,7 +17,7 @@ if (!empty($_POST) && $_POST['login_btn'] === 'login') {
   $post = $_POST;
   
   // バリデーションチェックをし、cssのclassセット
-  // $err_list = validation_check($validat);
+  $err_list = validation_check($validat);
   $class = empty($err_list) ? 'none' : '';
   if (!empty($class)) {
     $link = get_connect();
