224392c8b79863d192f4777908aa46164307afd3
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-11 10:21:56 +0900

    ログイン機能の実装

---
+++ customer/login.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once '../const.php';
+require_once '../func/func.php';
+require_once '../func/func_db.php';
+
+$class = 'none';
+
+
+// サインアップボタンを押下したときの処理
+if (!empty($_POST) && $_POST['login_btn'] === 'login') {
+
+  $post = $_POST;
+  
+  // バリデーションチェックをし、cssのclassセット
+  // $err_list = validation_check($validat);
+  $class = empty($err_list) ? 'none' : '';
+  if (!empty($class)) {
+    $link = get_connect();
+    
+    $params = [
+      'order' => ['id' => 'ASC']
+    ];
+    $list = run_select($link, 'v_auth', $params);
+    get_close($link);
+
+    if ( login_authentication($list, $post['email'], $post['password']) ) {
+      header('location: ../product/index.php');
+      exit;
+    }
+  }
+
+}
+
+
+require_once '../tpl/customer/login.php';
