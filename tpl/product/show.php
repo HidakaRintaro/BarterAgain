fc79b2e8cf9a61c5467de932760bbab8fee12042
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-15 01:19:38 +0900

    コメント機能の追加

--- tpl/product/show.php
+++ tpl/product/show.php
@@ -4,10 +4,12 @@
 <head>
   <title>header</title>
   <meta charset="utf-8">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js"></script>
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="../tpl/css/show.css">
   <link rel="stylesheet" href="../tpl/css/header.css">
+  <script src="../tpl/js/modal_show_script.js"></script>
 </head>
 
 <body>
@@ -22,7 +24,7 @@
       </ul>
     </nav>
   </header>
-
+  ​
   <div id="B">
     <br><br>
     <form action="#" method="GET">
@@ -33,66 +35,101 @@
     </form>
   </div>
 
+  <!-- ​エラーメッセージ -->
+  <div class="<?php echo $class; ?>">
+    <ul>
+<?php foreach ($err_list as $key => $err) : ?>
+      <li><?php echo $form_name[$key].$err_msg[$err]; ?></li>
+<?php endforeach; ?>
+    </ul>
+  </div>
+
   <!-- 商品詳細 -->
   <table>
     <tr>
       <th colspan="2">
-        <p><?php echo "商品名"; ?><p>
+        <p><?php echo $product['name']; ?><p>
       </th>
     <tr>
-
+      ​
     <tr>
       <td rowspan="4">
-        <p><img src="<?php echo FILE_IMG.$product['image_id']; ?>"></p>
+        <p><img src="<?php echo FILE_IMG . $product['image_id']; ?>"></p>
       </td>
       <td>
         <p>出品者　　　<?php echo $product['nickname']; ?></p>
       </td>
     </tr>
-
+    ​
     <tr>
       <td>
         <p>カテゴリー　<?php echo $product['category']; ?></p>
       </td>
     </tr>
-
+    ​
     <tr>
       <td>
         <p>ブランド　　<?php echo $product['brand']; ?></p>
       </td>
     </tr>
-
+    ​
     <tr>
       <td>
-        <p>商品の状態　<?php echo $status[$product['status']]; ?></p>
+        <p>商品の状態　<?php echo $product_status[$product['status']]; ?></p>
       </td>
     </tr>
-
-
+    ​
+    ​
     <!-- ボタン -->
-
-    <form method="get" action="">
+    ​
+    <div class="content">
       <tr>
-        <th colspan="2"><button class="btn1" type="submit" name="offer" value="offer">交換申請する</button></th>
+        <th colspan="2"><a class="js-modal-open" href=""><input class="btn1" name="request_btn" type="submit" value="交換申請する"></a></th>
       </tr>
-    </form>
-
+    </div>
+    <div class="modal js-modal">
+      <div class="modal__bg js-modal-close"></div>
+      <div class="modal__content">
+        <p>ここにモーダルウィンドウで表示したいコンテンツを入れます。モーダルウィンドウを閉じる場合は下の「閉じる」をクリックするか、背景の黒い部分をクリックしても閉じることができます。</p>
+        <a class="js-modal-close" href="">閉じる</a>
+      </div>
+      <!--modal__inner-->
+    </div>
+    <!--modal-->
+    ​
     <tr>
       <td colspan="2">
         <p><?php echo $product['body']; ?></p>
       </td>
     </tr>
-
+    ​
     <!-- コメント -->
-    <form method="get" action="../notice/index.php">
+    <form method="post">
       <tr>
-        <td colspan="2"><textarea name="comment" maxlength="500" rows="10" cols="80">コメント入力</textarea></td>
+        <td colspan="2"><textarea name="comment" maxlength="500" rows="10" cols="80" placeholder="コメント入力"></textarea></td>
       </tr>
       <br>
       <tr>
-        <th colspan="2"><input class="btn2" name="post_btn" type="submit" value="コメントする"></th>
+        <th colspan="2"><button class="btn2" name="post_btn" value="post_btn" type="submit">コメントする</button></th>
       </tr>
     </form>
+    <br>
+<?php foreach ($comment_arr as $val) : ?>
+      <tr>
+        <td><?php echo $val['nickname']; ?></td>
+        <td><?php echo $val['comment']; ?></td>
+<?php   if ($val['customer_id'] == $customer_id) : ?>
+          <td>
+            <form method="post">
+              <button type="submit" name="comment_del" value="comment_del">削除</button>
+              <input type="hidden" name="product_id" value="<?php echo $val['product_id']; ?>">
+              <input type="hidden" name="customer_id" value="<?php echo $val['customer_id']; ?>">
+              <input type="hidden" name="no" value="<?php echo $val['no']; ?>">
+            </form>
+          </td>
+<?php   endif; ?>
+      </tr>
+<?php endforeach; ?>
   </table>
 </body>
 
