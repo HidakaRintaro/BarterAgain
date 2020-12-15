ae5e0d2ab3c4feedf145fd221b79173147a7d771
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-15 12:37:22 +0900

    余白の修正

--- customer/sing_up.php
+++ customer/sing_up.php
@@ -23,16 +23,16 @@ $form_name = [
   'day' => '日'
 ];
 $validat = [
-  'nickname'        => ['blank' => 1, 'max_val' => 30],
-  'email'           => ['blank' => 1,                  'email' => 1, 'unique' => 'email'],
-  'password'        => ['blank' => 1, 'max_val' => 20, 'min_val' => 8],
-  'last_name'       => ['blank' => 1, 'max_val' => 20], 
-  'first_name'      => ['blank' => 1, 'max_val' => 20], 
-  'last_name_kana'  => ['blank' => 1, 'max_val' => 40, 'kana' => 1], 
-  'first_name_kana' => ['blank' => 1, 'max_val' => 40, 'kana' => 1], 
-  'year'            => ['blank' => 1], 
-  'month'           => ['blank' => 1], 
-  'day'             => ['blank' => 1] 
+  'nickname'        => [ 'blank' => 1, 'max_val' => 30 ],
+  'email'           => [ 'blank' => 1,                  'email' => 1, 'unique' => 'email' ],
+  'password'        => [ 'blank' => 1, 'max_val' => 20, 'min_val' => 8 ],
+  'last_name'       => [ 'blank' => 1, 'max_val' => 20 ], 
+  'first_name'      => [ 'blank' => 1, 'max_val' => 20 ], 
+  'last_name_kana'  => [ 'blank' => 1, 'max_val' => 40, 'kana' => 1 ], 
+  'first_name_kana' => [ 'blank' => 1, 'max_val' => 40, 'kana' => 1 ], 
+  'year'            => [ 'blank' => 1 ], 
+  'month'           => [ 'blank' => 1 ], 
+  'day'             => [ 'blank' => 1 ] 
 ];
 $err_msg = [
   'blank'   => 'が未入力です。',
@@ -49,24 +49,24 @@ $err_msg = [
 //------------------------------
 
 // 新規会員登録ボタンを押されたとき
-if (!empty($_POST) && $_POST['signup_btn'] === 'sing_up') {
+if ( !empty($_POST) && $_POST['signup_btn'] === 'sing_up' ) {
   $err_list = validation_check($validat);
   $class = empty($err_list) ? 'none' : '';
   
   // バリデーションエラーが無い時、DBに新規会員情報をINSERTする
-  if (!empty($class)) {
+  if ( !empty($class) ) {
     $post = $_POST;
 
     $link = get_connect();
 
     $insert_sql = [
-      'last_name'       => ['value'=> $post['last_name'],                                'type' => 's'], 
-      'first_name'      => ['value'=> $post['first_name'],                               'type' => 's'], 
-      'last_name_kana'  => ['value'=> $post['last_name_kana'],                           'type' => 's'], 
-      'first_name_kana' => ['value'=> $post['first_name_kana'],                          'type' => 's'], 
-      'email'           => ['value'=> $post['email'],                                    'type' => 's'], 
-      'birthday'        => ['value'=> $post['year'].'-'.$post['month'].'-'.$post['day'], 'type' => 's'], 
-      'nickname'        => ['value'=> $post['nickname'],                                 'type' => 's']
+      'last_name'       => [ 'value'=> $post['last_name'],                                'type' => 's' ], 
+      'first_name'      => [ 'value'=> $post['first_name'],                               'type' => 's' ], 
+      'last_name_kana'  => [ 'value'=> $post['last_name_kana'],                           'type' => 's' ], 
+      'first_name_kana' => [ 'value'=> $post['first_name_kana'],                          'type' => 's' ], 
+      'email'           => [ 'value'=> $post['email'],                                    'type' => 's' ], 
+      'birthday'        => [ 'value'=> $post['year'].'-'.$post['month'].'-'.$post['day'], 'type' => 's' ], 
+      'nickname'        => [ 'value'=> $post['nickname'],                                 'type' => 's' ]
     ];
     run_insert($link, 'customer', $insert_sql);
     $id = mysqli_insert_id($link);
@@ -75,10 +75,10 @@ if (!empty($_POST) && $_POST['signup_btn'] === 'sing_up') {
 
     $insert_sql = [];
     $insert_sql = [
-      'customer_id'        => ['value'=> $id,         'type' => 'i'], 
-      'encrypted_password' => ['value'=> $enc_arr[2], 'type' => 's'], 
-      'solt'               => ['value'=> $enc_arr[0], 'type' => 's'], 
-      'hash_cnt'           => ['value'=> $enc_arr[1], 'type' => 'i']
+      'customer_id'        => [ 'value'=> $id,         'type' => 'i' ], 
+      'encrypted_password' => [ 'value'=> $enc_arr[2], 'type' => 's' ], 
+      'solt'               => [ 'value'=> $enc_arr[0], 'type' => 's' ], 
+      'hash_cnt'           => [ 'value'=> $enc_arr[1], 'type' => 'i' ]
     ];
     run_insert($link, 'password_info', $params);
     get_close($link);
