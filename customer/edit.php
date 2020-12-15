8e3b01a714f2e38df1497c721ea3270fc6831fde
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-15 18:18:17 +0900

    会員更新ページの実装

--- customer/edit.php
+++ customer/edit.php
@@ -4,54 +4,91 @@ require_once '../const.php';
 require_once '../func/func.php';
 require_once '../func/func_db.php';
 
+// ログインチェック。未ログイン時ログイン画面に遷移する。
+is_login('../customer/login.php');
+$customer_id = intval($_SESSION['login']['customer_id']);
+
+
 $class = 'none';
-// バリデーションチェックを行うリスト
-// $validat = [
-//   'email'    => ['blank' => 1, 'email' => 1, 'unique' => 'email'],
-//   'password' => ['blank' => 1, 'max_val' => 20, 'min_val' => 8]
-// ];
+$form_name = [
+  'nickname'    => 'ニックネーム', 
+  'email'       => 'メールアドレス', 
+  'postal_code' => '郵便番号', 
+  'prefectures' => '都道府県', 
+  'address'     => '住所', 
+  'telephone'   => '電話番号'
+];
+$validat = [
+  'nickname'    => [ 'blank' => 1, 'max_val' => 30 ],
+  'email'       => [ 'blank' => 1, 'max_val' => 345, 'email' => 1, 'unique' => 'email' ], 
+  'postal_code' => [ 'blank' => 2,                   'digit' => 8 ], 
+  'prefectures' => [ 'blank' => 2 ], 
+  'address'     => [ 'blank' => 2, 'max_val' => 100 ], 
+  'telephone'   => [ 'blank' => 2,                   'digit' => 13 ], 
+];
+$err_msg = [
+  'blank'   => 'が未入力です。',
+  'max_val' => '',
+  'min_val' => '',
+  'email'   => 'を正しい形式で入力してください。',
+  'unique'  => 'このメールアドレスはすでに存在します。',
+  'digit'   => '△△は〇〇桁で入力してください。'
+];
+
+
+$link = get_connect();
+
+
+//------------------------------
+// 会員情報の更新
+//------------------------------
 
 // サインアップボタンを押下したときの処理
-if (!empty($_POST) && $_POST['update'] === 'update') {
-
+if ( !empty($_POST) && $_POST['update'] === 'update' ) {
   $post = $_POST;
-  
-  // バリデーションチェックをし、cssのclassセット
-  // $err_list = validation_check($validat);
-  // $class = empty($err_list) ? 'none' : '';
-  if (!empty($class)) {
-    $link = get_connect();
-    $params = [
+
+  $err_list = validation_check($validat);
+  $class = empty($err_list) ? 'none' : '';
+
+  // バリデーションエラーが無い時、DBに会員情報をUPDATEする
+  if ( !empty($class) ) {
+    $post['prefectures'] = empty($post['prefectures']) ? NULL : $post['prefectures'] ;
+    $update_sql = [
       'nickname'         => ['value' => $post['nickname'],    'type' => 's'], 
       'postal_code'      => ['value' => $post['postal'],      'type' => 's'], 
       'prefecture_id'    => ['value' => $post['prefectures'], 'type' => 's'], 
       'address'          => ['value' => $post['address'],     'type' => 's'], 
       'telephone_number' => ['value' => $post['telephone'],   'type' => 's'], 
       'email'            => ['value' => $post['email'],       'type' => 's'], 
-      'where'            => [ 'id = ?' => [ $_SESSION['login']['customer_id'] ] ]
+      'where'            => [ 'id = ?' => [ $customer_id ] ]
     ];
-    run_update($link, 'customer', $params);
-    get_close($link);
-    save_login_session($_SESSION['login']['customer_id']);
+    run_update($link, 'customer', $update_sql);
+    save_login_session($customer_id);
   }
 }
 
-$link = get_connect();
 
-$params = [
+//------------------------------
+// 会員情報の取得
+//------------------------------
+$select_sql = [
   'where' => [ 
-    'id = ?' => [ $_SESSION['login']['customer_id'] ] 
+    'id = ?' => [ $customer_id ] 
   ] 
 ];
-$list = run_select($link, 'v_customer_edit', $params);
-$list = $list[0];
+$list = run_select($link, 'v_customer_edit', $select_sql);
+$customer_info = $list[0];
 
-$params = [
-  'column' => ['id', 'name'], 
-  'order'  => ['id' => 'ASC' ]
+
+//------------------------------
+// 都道府県一覧の取得
+//------------------------------
+$select_sql = [
+  'column' => [ 'id', 'name' ], 
+  'order'  => [ 'id' => 'ASC' ]
 ];
-$prefecture_arr = run_select($link, 'prefecture', $params);
+$prefecture_arr = run_select($link, 'prefecture', $select_sql);
+
+
 get_close($link);
-
-
 require_once '../tpl/customer/edit.php';
--- tpl/customer/edit.php
+++ tpl/customer/edit.php
@@ -32,33 +32,42 @@
 
   <!-- 会員登録 -->
   <div id="torok">
+  <!-- ​エラーメッセージ -->
+    <div class="<?php echo $class; ?>">
+      <ul>
+<?php foreach ($err_list as $key => $err) : ?>
+        <li><?php echo $form_name[$key].$err_msg[$err]; ?></li>
+<?php endforeach; ?>
+      </ul>
+    </div>
     <form method="post">
       <h2>本人情報更新</h2>
-      <p>氏名：<?php echo $list['name']; ?></p>
-      <p>フリガナ：<?php echo $list['name_kana']; ?></p>
-      <p>生年月日：<?php echo $list['birthday']; ?></p>
-      <p>ニックネーム ：<input type="text" value="<?php echo $list['nickname']; ?>" name="nickname"></p>
+      <p>氏名：<?php echo $customer_info['name']; ?></p>
+      <p>フリガナ：<?php echo $customer_info['name_kana']; ?></p>
+      <p>生年月日：<?php echo $customer_info['birthday']; ?></p>
+      <p>ニックネーム ：<input type="text" value="<?php echo $customer_info['nickname']; ?>" name="nickname"></p>
+      <p>メールアドレス : <input type="text" name="email" value="<?php echo $customer_info['email']; ?>"></p>
       <br>
       <h2>発送元・お届け先住所変更</h2>
-      <p>郵便番号 : <input type="text" name="postal" value="<?php echo $list['postal_code']; ?>"></p>
+      <p>郵便番号 : <input type="text" name="postal" value="<?php echo $customer_info['postal_code']; ?>"></p>
       <br>
       <p>都道府県 : 
         <select name="prefectures">
           <option value="">--</option>
 <?php foreach ($prefecture_arr as $val) : ?>
-          <option value="<?php echo $val['id']; ?>" <?php echo $list['prefecture'] == $val['name'] ? 'selected' : '' ; ?>>
+          <option value="<?php echo $val['id']; ?>" <?php echo $customer_info['prefecture'] == $val['name'] ? 'selected' : '' ; ?>>
             <?php echo $val['name']; ?>
           </option>
 <?php endforeach; ?>
         </select>
       </p>
       <br>
-      <p>住所 : <input type="text" name="address" value="<?php echo $list['address']; ?>"></p>
+      <p>住所 : <input type="text" name="address" value="<?php echo $customer_info['address']; ?>"></p>
       <br>
-      <p>電話番号： <input type="tel" name="telephone" value="<?php echo $list['telephone_number'] ?>"> </p>
+      <p>電話番号： <input type="tel" name="telephone" value="<?php echo $customer_info['telephone_number'] ?>"> </p>
       <br>
       <!-- <h2>メールアドレス・パスワード</h2> -->
-      <p>メールアドレス : <input type="text" name="email" value="<?php echo $list['email']; ?>"></p>
+      <!-- <p>メールアドレス : <input type="text" name="email" value="<?php // echo $customer_info['email']; ?>"></p> -->
       <br>
       <!-- <p>現在のパスワード : <input type="password" name="old_password"></p>
       <br>
