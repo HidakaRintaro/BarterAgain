fa2c12ebb483656ce33efc819156b58255f66df4
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-16 03:27:25 +0900

    交換する商品一覧の表示

--- tpl/product/show.php
+++ tpl/product/show.php
@@ -75,20 +75,26 @@
     ​
     <div class="content">
       <tr>
-        <th colspan="2"><a class="js-modal-open" href=""><input class="btn1" name="request_btn" type="submit" value="交換申請する"></a></th>
+        <th colspan="2"><a class="js-modal-open" href=""><input class="btn1" name="btn" type="submit" value="交換する商品を選択"></a></th>
       </tr>
     </div>
     <div class="modal js-modal">
       <div class="modal__bg js-modal-close"></div>
       <div class="modal__content">
-        <p>交換する商品を選択</p>
+        <h2>交換する商品を選択</h2>
+        <br>
 <?php foreach ($barter_list as $row) : ?>
         <div class="modal_body">
-          <p><img src="<?php echo FILE_IMG.$row['image_id'] ?>" class="image"></p>
-          <p><?php echo $row['name'] ?></p>
-          <p>
-            <form method="post"></form>
-          </p>
+          <form method="post">
+            <p class="modal_img"><img src="<?php echo FILE_IMG.$row['image_id'] ?>" class="image"></p>
+            <p class="modal_product_name">
+              <?php echo $row['name'] ?>
+              <br>
+              <button type="submit" class="btn1" name="request_btn" value="request_btn">申請する</button>
+            </p>
+            <input type="hidden" name="barter_id" value="<?php echo $row['id'] ?>">
+            <input type="hidden" name="exhibit_id" value="<?php echo $product['id'] ?>">
+          </form>
         </div>
 <?php endforeach; ?>
         <a class="js-modal-close closeModal" href="">閉じる</a>
