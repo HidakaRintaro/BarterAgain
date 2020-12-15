6d34417b44c4f884c4591a33709c367cd23a38f8
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-16 00:30:04 +0900

    headerを1つにまとめる

---
+++ tpl/layout/header.php
@@ -0,0 +1,14 @@
+<header>
+  <h1>
+    <a>BarterAgain</a>
+  </h1>
+  <nav>
+    <ul id="hr_ul">
+      <li><a href="../product/index.php">HOME</a></li>
+      <li><a>カテゴリー</a></li>
+      <li><a href="../customer/my_pege.php">マイページ</a></li>
+      <!-- ログアウト処理 -->
+      <li><a href="">ログアウト</a></li>
+    </ul>
+  </nav>
+</header>
