53d1d51e1428be84872bbc1c6947ae5bea4462a6
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-12-16 20:45:59 +0900

    パスの修正

--- tpl/customer/my_pege.php
+++ tpl/customer/my_pege.php
@@ -7,8 +7,8 @@
   <!-- フォント -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="../css/header.css">
-  <link rel="stylesheet" href="../css/my_pege.css">
+  <link rel="stylesheet" href="../tpl/css/header.css">
+  <link rel="stylesheet" href="../tpl/css/my_pege.css">
 </head>
 
 <body>
@@ -30,19 +30,19 @@
   <div id="my_img">
     <table>
       <tr>
-        <td><a><img src="../img/my_pege/my1.JPG"></a>　</td>
-        <td><a><img src="../img/my_pege/my2.JPG"></a>　</td>
-        <td><a><img src="../img/my_pege/my3.JPG"></a>　</td>
+        <td><a><img src="../tpl/img/my_pege/my1.JPG"></a>　</td>
+        <td><a><img src="../tpl/img/my_pege/my2.JPG"></a>　</td>
+        <td><a><img src="../tpl/img/my_pege/my3.JPG"></a>　</td>
       </tr>
       <tr>
-        <td><a><img src="../img/my_pege/my4.JPG"></a>　</td>
-        <td><a><img src="../img/my_pege/my5.JPG"></a>　</td>
-        <td><a href="../notice/index.php"><img src="../img/my_pege/my6.JPG"></a>　</td>
+        <td><a><img src="../tpl/img/my_pege/my4.JPG"></a>　</td>
+        <td><a><img src="../tpl/img/my_pege/my5.JPG"></a>　</td>
+        <td><a href="../notice/index.php"><img src="../tpl/img/my_pege/my6.JPG"></a>　</td>
       </tr>
       <tr>
-        <td><a href="../comment/index.php"><img src="../img/my_pege/my7.JPG"></a>　</td>
-        <td><a href="../nice/index.php"><img src="../img/my_pege/my8.JPG"></a>　</td>
-        <td><a><img src="../img/my_pege/my9.JPG"></a>　</td>
+        <td><a href="../comment/index.php"><img src="../tpl/img/my_pege/my7.JPG"></a>　</td>
+        <td><a href="../nice/index.php"><img src="../tpl/img/my_pege/my8.JPG"></a>　</td>
+        <td><a><img src="../tpl/img/my_pege/my9.JPG"></a>　</td>
       </tr>
   </div>
   </table>
